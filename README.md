ec890fefa434cc153b9fa3c1a068e27f1ee0cd0f
qcryptic <kylerump@outlook.com>
2022-06-22 15:50:44 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,38 +1,49 @@
 # Photo Metadata Updater and Geotagger
 
-Easily update and geotag picture metadata, with image view and map view.
+Allows you to view and update photo metadata, timezone, and geotag/coordinates/location.
 
-Usage for running locally only.
+* Includes support for viewing equirectangular/360 photos
+* Formats metadata to fix issues with google photos so photos stay in sync
 
-# Libraries Used
+Node.js application with a Vue frontend. Runs on Mac/Unix systems, not tested on Windows. This app is only made to be run locally. 
 
-* [Pannellum](https://pannellum.org/) - equirectangular image projection viewer
-* [Bing Maps](https://docs.microsoft.com/en-us/bingmaps/v8-web-control/creating-and-hosting-map-controls/) - map view
-    - Requires API key, store it in your `.env` file with name `BING_API_KEY`
+**Please make backups of all photos before using the app and editing metadata, in case of unexpected errors.**
 
+# Required Dependencies
 
-# Dependencies
+Ensure you have the following dependencies downloaded on your system:
 
-Requires [exiftool](https://exiftool.org/install.html) to be installed and globally accessable on your system.
+* [exiftool](https://exiftool.org/install.html) - Tool used to view and edit photo metadata
+* [Node.js](https://nodejs.org/en/download/) - Used to run the application
+* [Bing maps](https://docs.microsoft.com/en-us/bingmaps/v8-web-control/creating-and-hosting-map-controls/) - API key required (free)
 
+# Libraries
 
-# Build app
+* [Vue.js](https://vuejs.org/) - Frontend framework
+* [Vuetify](https://vuetifyjs.com/en/) - Vue UI library based on Google's material design
+* [Pannellum](https://pannellum.org/) - Equirectangular image projection viewer
+* [Bing Maps](https://docs.microsoft.com/en-us/bingmaps/v8-web-control/creating-and-hosting-map-controls/) - Map view and timezones API
+* [Express.js](https://expressjs.com/) - Web framework for Node apps
 
-This has only been tested on Mac, but should work on Windows/Unix:
+# Build and Run Application
 
-1. clone repo
+### Download application
+1. Clone this repo
+2. From the root directory of the cloned repo execute these commands:
+    - `npm install` - Installs Nodejs dependencies
+    - `npm run install-ui` - Installs the frontend dependencies
+    - `npm run build-ui` - Builds/Compiles the frontend into the `public/` folder
 
-2. cd to directory with repo
+### Create properies file 
+1. From the root directory create a file called `.env`
+2. Add the following properties:
+```
+BING_API_KEY={your API key}
+EXIFTOOL_PATH={/path/to/exiftool}
+```
+* EXIFTOOL_PATH is optional and not needed if EXIFTOOL is accessable globally on your system
 
-3. npm install
-
-4. npm run install-ui
-
-5. npm run build-ui
-
-
-# Run app
-
-Use `npm start` from the project directory or use `node <path/to/photo-metadata-updater>` from any directory
+### Run application
+Use `npm start` from the project directory or use `node <path/to/photo-metadata-updater.js>` from any directory
 
 App should be accessible on http://localhost:8000
--- src/service/exiftool-service.js
+++ src/service/exiftool-service.js
@@ -2,12 +2,14 @@ const shell = require('shelljs');
 const asyncShell = require('../utils/async-shell');
 const coordinatesUtils = require('../utils/coordinates-utils');
 
+const exiftool = process.env.EXIFTOOL_PATH || 'exiftool';
+
 module.exports = {
 	setGeotag: function(file, lat, lon) {
 		if (coordinatesUtils.isValidCoordinates(lat, lon)) {
 			let latRef = (lat < 0) ? 'S' : 'N';
 			let lonRef = (lon < 0) ? 'W' : 'E';
-			let cmd = `exiftool "${file}" -gpslatitude=${lat}  -gpslongitude=${lon} -gpslatituderef=${latRef} -gpslongituderef=${lonRef} -overwrite_original -q -q`;
+			let cmd = `${exiftool} "${file}" -gpslatitude=${lat}  -gpslongitude=${lon} -gpslatituderef=${latRef} -gpslongituderef=${lonRef} -overwrite_original -q -q`;
 			if (shell.exec(cmd).code === 0) {
 				return true;
 			}
@@ -19,7 +21,7 @@ module.exports = {
   setDateTime: function(file, date, time) {
     if (/[0-9]{4}-[0-9]{2}-[0-9]{2}/.test(date) && /[0-9]{2}:[0-9]{2}:[0-9]{2}/.test(time)) {
       const datetime = `${date.replaceAll("-", ":")} ${time}`;
-      const cmd = `exiftool "${file}" -datetimeoriginal="${datetime}" -createdate="${datetime}" -overwrite_original -q -q`;
+      const cmd = `${exiftool} "${file}" -datetimeoriginal="${datetime}" -createdate="${datetime}" -overwrite_original -q -q`;
       if (shell.exec(cmd).code === 0) {
         return true;
       }
@@ -28,7 +30,7 @@ module.exports = {
   },
 
   setCamera: function(file, camera) {
-    const cmd = `exiftool "${file}" -model="${camera}" -overwrite_original -q -q`;
+    const cmd = `${exiftool} "${file}" -model="${camera}" -overwrite_original -q -q`;
     if (shell.exec(cmd).code === 0) {
       return true;
     }
@@ -40,7 +42,7 @@ module.exports = {
     elevation = elevation.replaceAll('m', '');
     elevation = elevation.replaceAll('M', '');
     const formattedElevation = `${elevation} m ${direction}`;
-    const cmd = `exiftool "${file}" -gpsaltitude=="${formattedElevation}" -overwrite_original -q -q`;
+    const cmd = `${exiftool} "${file}" -gpsaltitude=="${formattedElevation}" -overwrite_original -q -q`;
     if (shell.exec(cmd).code === 0) {
       return true;
     }
@@ -49,22 +51,22 @@ module.exports = {
 
 	getMetaData: async function(file) {
 		let infoMap = new Map();
-		let data = await asyncShell.exec(`exiftool "${file}"`).catch(err => {throw err});
+		let data = await asyncShell.exec(`${exiftool} "${file}"`).catch(err => {throw err});
 		data.split(/\r?\n/).forEach((item) => {
 			let tmp = item.split(': ');
 			infoMap.set(tmp[0].trim(), tmp[1]);
 		});
     const {date, time} = extractDateTime(infoMap.get('Create Date'));
 		let res = { 
-			'name': infoMap.get('File Name'), 
+			'name': infoMap.get('File Name') || 'unknown', 
 			'path': file,
-			'type': infoMap.get('File Type'),
-			'size': infoMap.get('File Size'),
-			'camera': infoMap.get('Camera Model Name'),
+			'type': infoMap.get('File Type') || 'unknown',
+			'size': infoMap.get('File Size') || 'unknown',
+			'camera': infoMap.get('Camera Model Name') || 'unknown',
 			'createDate': date,
       'createTime': time,
-			'resolution': infoMap.get('Image Size'),
-			'projection': infoMap.get('Projection Type'),
+			'resolution': infoMap.get('Image Size') || 'unknown',
+			'projection': infoMap.get('Projection Type') || 'unknown',
 			'coordinates': coordinatesUtils.geotagToCoordinates(infoMap.get('GPS Position')),
       'elevation': mapElevation(infoMap.get('GPS Altitude') || '0 m Above') 
 		};
@@ -75,6 +77,9 @@ module.exports = {
 }
 
 function extractDateTime(created) {
+  if (created === undefined) {
+    return {"date": "1970-01-01", "time": "00:00:00"};
+  }
   let split = created.split(" ");
   return {
     "date": split[0].replaceAll(":", "-"),
